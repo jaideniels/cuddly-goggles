75c3baf5ca1fee4d3254357688f7dfd4d5f83c24
Jay Daniels <jaymdan@gmail.com>
2020-05-10 11:24:23 -0700

    not a good name...

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=stacks.py
+FLASK_DEBUG=1
---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+@app.route('/')
+def index():
+    return '<h1>hello world</h1>'
