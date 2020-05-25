1e792a1cb53937197b8796a411a9c4ba2a7d0d62
Jay Daniels <jaymdan@gmail.com>
2020-05-25 12:59:43 -0700

    many-to-many table and dummy data

--- app/models.py
+++ app/models.py
@@ -4,3 +4,28 @@ from app import db
 class Something(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(128))
+
+
+class UserStack(db.Model):
+    __tablename__ = 'user_stack'
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    stack_id = db.Column(db.Integer, db.ForeignKey('stack.id'), primary_key=True)
+
+    user = db.relationship("User", back_populates="stacks")
+    stack = db.relationship("Stack", back_populates="users")
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+
+    stacks = db.relationship("UserStack", back_populates="user")
+
+
+class Stack(db.Model):
+    __tablename__ = 'stack'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+
+    users = db.relationship("UserStack", back_populates="stack")
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,5 @@
 from app import app, db
-from .models import Something
-
+from .models import Something, User, Stack, UserStack
 
 @app.route('/')
 def index():
@@ -10,4 +9,26 @@ def index():
 @app.route('/createdb/')
 def create():
     db.create_all()
+    return "created!"
+
+
+@app.route('/recreatedb/')
+def recreate():
+    db.drop_all()
+    db.create_all()
+
+    # dummy table records
+    something = Something(data="hi jay!")
+    db.session.add(something)
+
+    # stacks data
+    user = User(name="jay")
+    stack = Stack(name="colors")
+    userstack = UserStack()
+    userstack.stack = stack
+    userstack.user = user
+    db.session.add(userstack)
+
+    # commit the changes
+    db.session.commit()
     return "created!"
