7dc545607cebbaa9a864b4d248f73d7f3684c633
jaideniels <47468949+jaideniels@users.noreply.github.com>
2020-05-26 19:50:30 -0700

    marshmallow (#7)
    add marshmallow
    add smorest
    new models and schemas
    basic routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
+from flask_smorest import Api
+
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+marsh = Marshmallow(app)
 migrate = Migrate(app, db)
+api = Api(app)
 
 from app import routes  # noqa: E402 F401
+
+api.register_blueprint(routes.blp)
