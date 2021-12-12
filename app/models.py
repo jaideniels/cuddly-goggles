ecf4e6cbca3217340f8ea4b6390483bb0eb5ed5e
Jay Daniels <jaymdan@gmail.com>
2021-12-11 16:47:10 -0800

    post cards and hangul sample data

--- app/models.py
+++ app/models.py
@@ -15,9 +15,6 @@ class User(db.Model):
     stacks = association_proxy('user_stacks', 'stack',  creator=lambda stack: UserStack(stack=stack))
     scores = db.relationship('Score', back_populates='user')
 
-    def __init__(self, name):
-        self.name = name
-
 
 class Stack(db.Model):
     __tablename__ = 'stack'
@@ -31,9 +28,6 @@ class Stack(db.Model):
     # Reliationships
     users = association_proxy('stack_users', 'user', creator=lambda user: UserStack(user=user))
     cards = association_proxy('stack_cards', 'card', creator=lambda card: StackCard(card=card))
-
-    def __init__(self, name):
-        self.name = name
 
 
 class Card(db.Model):
@@ -49,9 +43,6 @@ class Card(db.Model):
     stacks = association_proxy('card_stacks', 'stack', creator=lambda stack: StackCard(stack=stack))
     clues = db.relationship('Clue', back_populates='card')
     facts = db.relationship('Fact', back_populates='card')
-
-    def __init__(self, name):
-        self.name = name
 
 
 class Fact(db.Model):
@@ -70,8 +61,6 @@ class Fact(db.Model):
     card = db.relationship('Card', back_populates='facts')
     clues = association_proxy('fact_clues', 'clue', creator=lambda clue: ClueFact(clue=clue))
 
-    def __init__(self, fact):
-        self.fact = fact
 
 
 class Clue(db.Model):
@@ -88,9 +77,6 @@ class Clue(db.Model):
     scores = db.relationship('Score', back_populates='clue')
     facts = association_proxy('clue_facts', 'fact', creator=lambda fact: ClueFact(fact=fact))
 
-    def __init__(self, facts):
-        for fact in facts:
-            self.facts.append(fact)
 
 
 class Game(db.Model):
@@ -105,8 +91,6 @@ class Game(db.Model):
     # Relationships
     scores = db.relationship('Score', back_populates='game')
 
-    def __init__(self, name):
-        self.name = name
 
 
 class Score(db.Model):
@@ -128,11 +112,6 @@ class Score(db.Model):
     clue = db.relationship('Clue', back_populates='scores')
     game = db.relationship('Game', back_populates='scores')
 
-    def __init__(self, score, user=None, clue=None, game=None):
-        self.score = score
-        self.user = user
-        self.clue = clue
-        self.game = game
 
 
 class UserStack(db.Model):
@@ -146,9 +125,6 @@ class UserStack(db.Model):
     user = db.relationship('User',  backref=db.backref('user_stacks', cascade='all, delete-orphan'))
     stack = db.relationship('Stack', backref=db.backref('stack_users', cascade='all, delete-orphan'))
 
-    def __init__(self, user=None, stack=None):
-        self.user = user
-        self.stack = stack
 
 
 class StackCard(db.Model):
@@ -162,10 +138,6 @@ class StackCard(db.Model):
     stack = db.relationship('Stack', backref=db.backref('stack_cards', cascade='all, delete-orphan'))
     card = db.relationship('Card', backref=db.backref('card_stacks', cascade='all, delete-orphan'))
 
-    def __init__(self, stack=None, card=None):
-        self.stack = stack
-        self.card = card
-
 
 class ClueFact(db.Model):
     __tablename__ = 'clue_fact'
@@ -177,7 +149,3 @@ class ClueFact(db.Model):
     # Relaiionships
     clue = db.relationship('Clue', backref=db.backref('clue_facts', cascade='all, delete-orphan'))
     fact = db.relationship('Fact', backref=db.backref('fact_clues', cascade='all, delete-orphan'))
-
-    def __init__(self, clue=None, fact=None):
-        self.clue = clue
-        self.fact = fact
